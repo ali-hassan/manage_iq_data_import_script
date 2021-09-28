cb3a159cc4854cf0a923e0106b98a02851ab0a0a
Ali Hassan Mirza <ali.hassan.mirzaa+100@gmail.com>
2021-09-28 14:35:28 -0400

    Fixed bugs

--- csv_reader.rb
+++ csv_reader.rb
@@ -2,7 +2,6 @@ puts "Started Parsing"
 require 'smarter_csv'
 require "byebug"
 
-
 data = File.open("updated_sheet_final_version.csv")
 index = 0;
 
@@ -15,41 +14,70 @@ CSV.foreach("updated_sheet_final_version.csv").map do |row|
   row.each do |col_val|
     cat_name = col_val
     category_id = nil
-
+    cat_name_val = cat_name.downcase.gsub(/[^0-9A-Za-z]/, '_')
     response = `curl -v -k -u admin:WSXQAZ@655#@! --location --request POST 'https://172.31.211.137/api/categories' \
                    --header 'Authorization: Basic YWRtaW46V1NYUUFaQDY1NSNAIQ==' \
                    --header 'Content-Type: text/plain'\
                    --data-raw '{
-                   "name" : "#{cat_name.downcase.tr(" ", "_")}",
+                   "name" : "#{cat_name_val}",
                   "description" : "#{cat_name}"
                  }'`
-    response = eval(response)
-    is_error =  response[:error]
-    category_id = response[:id] unless is_error
+    response = JSON.parse(response)
+    puts "response is pasted here *******************************************************"
+    puts "response is = #{response}"
+    is_error =  response["error"]
+    category_id = response["id"] unless is_error
 
     if is_error
-      categories = `curl -v -k -u admin:WSXQAZ@655#@! --location --request GET 'https://172.31.211.137/api/categories'`
+      all_categories = `curl -v -k -u admin:WSXQAZ@655#@! --location --request GET 'https://172.31.211.137/api/categories'`
       puts "categories registered alread"
       puts "searching appropiate category"
-      eval(categories)[:resources].map do |cat|
-        response = `curl -v -k -u admin:WSXQAZ@655#@! --location --request GET "cat[:href]" \
+      categories = JSON.parse(all_categories)
+
+      categories["resources"].map do |cat|
+        response = `curl -v -k -u admin:WSXQAZ@655#@! --location --request GET "#{cat["href"]}" \
             --header 'Authorization: Basic YWRtaW46V1NYUUFaQDY1NSNAIQ==' \
             --header 'Content-Type: text/plain' \
            --data-raw '{
-           "name" : "test",
-          "description" : "Test Category"
+           "name" : "#{cat_name_val}",
+          "description" : "#{cat_name}"
           }'`
-        response = eval(response)
-        
-        if response[:description] == cat_name
+        response = JSON.parse(response)
+        if response["description"] == cat_name
           puts "Relvant category found successfully and its matched"
-          puts "category is #{response[:description]}"
-          category_id = response[:id]
+          puts "category is #{response["description"]}"
+          category_id = response["id"]
+          puts "categories found."
           break
         end
       end
+
       CSV.parse(File.open('updated_sheet_final_version.csv'), headers: true).by_col[col_val].each do |val|
         #request.body = "{\r\n  \"name\" : \"#{val.downcase.tr(" ", "_")}\",\r\n  \"description\" : \"#{val}\"\r\n}"
+        tag_name = val
+        tag_name = val.downcase.gsub!(/[^0-9A-Za-z]/, '_') unless cat_name_val == "sr_no"
+        puts "tag_name is=#{tag_name}*********************************************************************************"
+        response = `curl -v -k -u admin:WSXQAZ@655#@! --location --request POST "https://172.31.211.137/api/categories/#{category_id}/tags" \
+     --header 'Authorization: Basic YWRtaW46V1NYUUFaQDY1NSNAIQ==' \
+     --header 'Content-Type: text/plain' \
+     --data-raw '{
+      "name" : "#{tag_name}",
+     "description" : "#{val}"
+    }'`
+        response = JSON.parse(response)
+        puts "*********************************Tag response =#{response}**********************"
+        if response["error"]
+          puts "Tag creation error is below:"
+          puts response[:error]
+        end
+      end
+    else
+      CSV.parse(File.open('updated_sheet_final_version.csv'), headers: true).by_col[col_val].each do |val|
+        #request.body = "{\r\n  \"name\" : \"#{val.downcase.tr(" ", "_")}\",\r\n  \"description\" : \"#{val}\"\r\n}"
+        puts "***************************************************************************************************************************"
+        puts "category_id=#{category_id}"
+        puts "****************************************************************************************************************************"
+
         response = `curl -v -k -u admin:WSXQAZ@655#@! --location --request POST "https://172.31.211.137/api/categories/#{category_id}/tags" \
        --header 'Authorization: Basic YWRtaW46V1NYUUFaQDY1NSNAIQ==' \
        --header 'Content-Type: text/plain' \
@@ -57,16 +85,18 @@ CSV.foreach("updated_sheet_final_version.csv").map do |row|
         "name" : "#{val.downcase.tr(" ", "_")}",
        "description" : "#{val}"
       }'`
-        response = eval(request)
-        if response[:error]
+        response = JSON.parse(request)
+        puts "********************************************************************************************************************************"
+        puts "response of tag is=#{response}"
+        if response["error"]
           puts "Tag creation error is below:"
           puts response[:error]
         end
       end
 
     end
+
   end
 end
 
 puts "End Parsing"
-
