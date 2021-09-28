ef59ef6016d159cd01f35f0853672860f3b71a99
Ali Hassan Mirza <ali.hassan.mirzaa+100@gmail.com>
2021-09-28 11:11:43 -0400

    Changes

--- csv_reader.rb
+++ csv_reader.rb
@@ -6,12 +6,6 @@ require "byebug"
 data = File.open("updated_sheet_final_version.csv")
 index = 0;
 
-require "uri"
-require "net/http"
-
-#response = https.request(request)
-#puts response.read_body
-
 puts "Get all columns of CSV"
 
 CSV.foreach("updated_sheet_final_version.csv").map do |row|
@@ -20,15 +14,7 @@ CSV.foreach("updated_sheet_final_version.csv").map do |row|
 
   row.each do |col_val|
     cat_name = col_val
-
-    # url = URI("https://172.31.211.137/api/categories")
-    # https = Net::HTTP.new(url.host, url.port)
-    # https.use_ssl = true
-    # request = Net::HTTP::Post.new(url)
-    # request["Authorization"] = "Basic YWRtaW46V1NYUUFaQDY1NSNAIQ=="
-    # request["Content-Type"] = "text/plain"
-    # request.body = "{\r\n  \"name\" : \"#{cat_name.downcase.tr(" ", "_")}\",\r\n  \"description\" : \"#{cat_name}\"\r\n}"
-
+    category_id = nil
 
     response = `curl -v -k -u admin:WSXQAZ@655#@! --location --request POST 'https://172.31.211.137/api/categories' \
                    --header 'Authorization: Basic YWRtaW46V1NYUUFaQDY1NSNAIQ==' \
@@ -37,54 +23,48 @@ CSV.foreach("updated_sheet_final_version.csv").map do |row|
                    "name" : "#{cat_name.downcase.tr(" ", "_")}",
                   "description" : "#{cat_name}"
                  }'`
-    get_re= #`curl -v -k -u admin:WSXQAZ@655#@! --location --request GET 'https://172.31.211.137/api/categories/344'`
-    puts response.read_body
-    category_id = response.read_body["id"]
-    debugger
+    response = eval(response)
+    is_error =  response[:error]
+    category_id = response[:id] unless is_error
 
-
-
-
-    response = https.request(request)
-    puts response.read_body
-    category_id = response.read_body["id"]
-
-    unless category_id.present?
-      # url = URI("https://172.31.211.137/api/categories")
-      #
-      # https = Net::HTTP.new(url.host, url.port)
-      # https.use_ssl = true
-      #
-      # request = Net::HTTP::Get.new(url)
-      # request["Authorization"] = "Basic YWRtaW46V1NYUUFaQDY1NSNAIQ=="
-      # request["Content-Type"] = "text/plain"
-      # request.body = "{\r\n  \"name\" : \"#{cat_name.downcase.tr(" ", "_")}\",\r\n  \"description\" : \"#{cat_name}\"\r\n}"
-
+    if is_error
       categories = `curl -v -k -u admin:WSXQAZ@655#@! --location --request GET 'https://172.31.211.137/api/categories'`
-      categories.map do |cat|
-        puts cat
+      puts "categories registered alread"
+      puts "searching appropiate category"
+      eval(categories)[:resources].map do |cat|
+        response = `curl -v -k -u admin:WSXQAZ@655#@! --location --request GET "cat[:href]" \
+            --header 'Authorization: Basic YWRtaW46V1NYUUFaQDY1NSNAIQ==' \
+            --header 'Content-Type: text/plain' \
+           --data-raw '{
+           "name" : "test",
+          "description" : "Test Category"
+          }'`
+        response = eval(response)
+        
+        if response[:description] == cat_name
+          puts "Relvant category found successfully and its matched"
+          puts "category is #{response[:description]}"
+          category_id = response[:id]
+          break
+        end
       end
-      puts response.read_body
-      category_id = response.read_body["id"]
+      CSV.parse(File.open('updated_sheet_final_version.csv'), headers: true).by_col[col_val].each do |val|
+        #request.body = "{\r\n  \"name\" : \"#{val.downcase.tr(" ", "_")}\",\r\n  \"description\" : \"#{val}\"\r\n}"
+        response = `curl -v -k -u admin:WSXQAZ@655#@! --location --request POST "https://172.31.211.137/api/categories/#{category_id}/tags" \
+       --header 'Authorization: Basic YWRtaW46V1NYUUFaQDY1NSNAIQ==' \
+       --header 'Content-Type: text/plain' \
+       --data-raw '{
+        "name" : "#{val.downcase.tr(" ", "_")}",
+       "description" : "#{val}"
+      }'`
+        response = eval(request)
+        if response[:error]
+          puts "Tag creation error is below:"
+          puts response[:error]
+        end
+      end
+
     end
-
-    # Remove first element of the array
-
-    CSV.parse(File.open('updated_sheet_final_version.csv'), headers: true).by_col[col_val].each do |val|
-
-      url = URI("https://172.31.211.137/api/categories/#{category_id}/tags")
-      https = Net::HTTP.new(url.host, url.port)
-      https.use_ssl = true
-
-      request = Net::HTTP::Post.new(url)
-      request["Authorization"] = "Basic YWRtaW46V1NYUUFaQDY1NSNAIQ=="
-      request["Content-Type"] = "text/plain"
-      request.body = "{\r\n  \"name\" : \"#{val.downcase.tr(" ", "_")}\",\r\n  \"description\" : \"#{val}\"\r\n}"
-
-      response = https.request(request)
-      puts response.read_body
-    end
-
   end
 end
 
