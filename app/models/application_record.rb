d5e8fa2e3a92dd2a590be992cad0599536d4f9ad
Ali Hassan Mirza <ali.hassan.mirzaa+100@gmail.com>
2021-10-12 11:22:13 -0400

    some changes in code

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,4 +1,12 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 
+  class << self
+    ignore_key = false
+    get_replacements = false
+    is_captured = false
+    is_public = true
+    is_deprecated = true
+
+  end
 end
