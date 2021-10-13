87f3d103c1f884c8053feb9ad2636682d47c353c
Ali Hassan Mirza <ali.hassan.mirzaa+100@gmail.com>
2021-10-13 01:59:50 -0400

    changes the things

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -2,11 +2,31 @@ class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 
   class << self
-    ignore_key = false
-    get_replacements = false
-    is_captured = false
-    is_public = true
-    is_deprecated = true
+    ignore_key         = false
+    get_replacements   = false
+    is_captured        = false
+    is_public          = true
+    is_deprecated      = true
+  end
 
+
+  ### Captured functionality #####
+  def captured
+    if self.is_captured
+      return self
+    else
+      self.is_captured = true
+    end
+  end
+
+  def public
+    if self.is_public
+      return false
+    end
+  end
+  ### Warnings #####
+  ### Deprecated ###
+  def deprecated
+    self.is_deprecated ? false : true
   end
 end
